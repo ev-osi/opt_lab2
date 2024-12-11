5d8205db1d1e709d922769c260c252b8e72ffb41
ev-osi <ev.osinkin@mail.ru>
2024-12-11 20:53:17 +0300

    123456789012345678901234567

--- lab6-linux-client.cpp
+++ lab6-linux-client.cpp
@@ -2,7 +2,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
-#include <sys/statvfs.h>
 #include <unistd.h>
 #include <cstdio>
 #include <cstdlib>
@@ -24,63 +23,66 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    int serverSocket;
+    int clientSocket;
     struct sockaddr_in serverAddr;
     char buffer[BUFFER_SIZE];
     char path[BUFFER_SIZE];
-    socklen_t addrLen = sizeof(serverAddr);
+    socklen_t serverAddrLen = sizeof(serverAddr);
 
-    serverSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (serverSocket < 0) {
+    clientSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (clientSocket < 0) {
         perror("Socket creation failed");
         exit(1);
     }
 
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(port);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Bind failed");
-        close(serverSocket);
+    if (inet_pton(AF_INET, server_ip, &serverAddr.sin_addr) <= 0) {
+        fprintf(stderr, "Invalid address or address not supported.\n");
+        close(clientSocket);
         exit(1);
     }
 
-    printf("Server running at %s:%d\n", server_ip, port);
+    // Output connection message
+    printf("Connecting to server at %s:%d\n", server_ip, port);
 
     while (true) {
-        int recvLen = recvfrom(serverSocket, buffer, BUFFER_SIZE, 0,
-                               (struct sockaddr *)&serverAddr, &addrLen);
+        printf("Enter the path to check (or 'exit' to quit): ");
+        fgets(path, BUFFER_SIZE, stdin);
+        path[strcspn(path, "\n")] = '\0';
+
+        if (strcmp(path, "exit") == 0) {
+            snprintf(buffer, BUFFER_SIZE, "Exiting.");
+            sendto(clientSocket, buffer, strlen(buffer), 0,
+                   (struct sockaddr *)&serverAddr, serverAddrLen);
+            break;
+        }
+
+        if (access(path, F_OK) == -1) {
+            perror("Path does not exist");
+            snprintf(buffer, BUFFER_SIZE, "Error: Path does not exist");
+            sendto(clientSocket, buffer, strlen(buffer), 0,
+                   (struct sockaddr *)&serverAddr, serverAddrLen);
+            continue;
+        }
+
+        if (sendto(clientSocket, path, strlen(path), 0,
+                   (struct sockaddr *)&serverAddr, serverAddrLen) == -1) {
+            perror("Failed to send data");
+            continue;
+        }
+
+        int recvLen = recvfrom(clientSocket, buffer, BUFFER_SIZE, 0,
+                               (struct sockaddr *)&serverAddr, &serverAddrLen);
         if (recvLen == -1) {
             perror("Failed to receive data");
             continue;
         }
 
         buffer[recvLen] = '\0';
-        printf("Received request for path: %s\n", buffer);
-
-        if (strcmp(buffer, "exit") == 0) {
-            snprintf(buffer, BUFFER_SIZE, "Exiting.");
-            sendto(serverSocket, buffer, strlen(buffer), 0,
-                   (struct sockaddr *)&serverAddr, addrLen);
-            break;
-        }
-
-        if (access(buffer, F_OK) == -1) {
-            snprintf(buffer, BUFFER_SIZE, "Error: Path does not exist");
-        } else {
-            struct statvfs stat;
-            if (statvfs(buffer, &stat) == -1) {
-                snprintf(buffer, BUFFER_SIZE, "Error: Failed to retrieve disk status");
-            } else {
-                snprintf(buffer, BUFFER_SIZE, "Server response: Free: %lu, Used: %lu", 
-                         stat.f_bfree * stat.f_frsize, (stat.f_blocks - stat.f_bfree) * stat.f_frsize);
-            }
-        }
-
-        sendto(serverSocket, buffer, strlen(buffer), 0,
-               (struct sockaddr *)&serverAddr, addrLen);
+        printf("Server response: %s\n", buffer);
     }
 
-    close(serverSocket);
+    close(clientSocket);
+    return 0;
 }
